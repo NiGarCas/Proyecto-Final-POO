5e8d78c1d2fe591b22171a4381b36d1b18bf82df
nicol <nicol@LAPTOP-J0EGA5H6>
2017-11-10 15:44:06 -0500

    Separación Datos, lógica y visualización (Modelo-Vista-Controlador)

--- src/datos/Jugador.java
+++ src/datos/Jugador.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package adivina.el.futbolista;
+package datos;
 
+import datos.Equipo;
 import java.util.ArrayList;
 
 /**
--- src/logica/MainProvisional.java
+++ src/logica/MainProvisional.java
@@ -1,10 +1,15 @@
+package logica;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package adivina.el.futbolista;
 
+
+import datos.Nivel;
+import datos.Jugador;
+import datos.Equipo;
 import java.util.ArrayList;
 
 /**
